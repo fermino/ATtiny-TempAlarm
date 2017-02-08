330a3d5eab101febb957324c5f0f10ae07c2ca62
Fermín Olaiz <ferminolaiz@gmail.com>
2017-02-08 18:19:07 -0300

    Configuration refactor (thats preeety (?)

--- TempAlarm/Configuration.cpp
+++ TempAlarm/Configuration.cpp
@@ -2,110 +2,30 @@
 
 	/**
 	 * BEGIN CONFIG
+	 * 
+	 * To do: 
+	 * Correct some names
+	 * Add some defines, as the time between sensor's reads
 	 */
 
-	// Project's pinout
+	/**
+	 * LCD
+	 * 
+	 * Pinout: 
+	 * SDA <=> ATtiny's pin 5 (Digital IO #0)
+	 * SCL <=> ATtiny's pin 7 (Digital IO #2)
+	 * 
+	 * Here you must put the LCD config and features, as the I2C address, columns
+	 * and rows. If it have more than 2 rows, the Timer module will be compiled. 
+	 * 
+	 * Also, here are defined some characters that are used widely in the firmware, 
+	 * you can change them if you want :)
+	 * 
+	 * The alarm's direction means if the sensor should be heated or cooled for
+	 * the alarm to be activated. 
+	 */
 
-		/** LCD pinout
-		 * SDA <=> ATtiny's pin 5 (Digital IO #0)
-		 * SCL <=> ATtiny's pin 7 (Digital IO #2)
-		 */
-
-		// An active buzzer
-		#define BUZZER_PIN 1
-
-		// Data pin of temperature sensor
-		// Disturbs the SPI signals while programming if connected to pin 2 (or other SPI pin) 
-		#define ONE_WIRE_BUS 3
-
-		// OneWireSwitches input pin
-		#define SWITCHES_INPUT_PIN A2
-
-	// OneWireSwitches configuration
-
-		// This works by dividing voltages on only one analog input
-		// See https://learn.sparkfun.com/tutorials/voltage-dividers
-
-		// How many switches do we have?
-		#define SWITCHES_AMOUNT 8
-
-		// Define array indexes to make the code prettier :P
-		#define CHANGE_DIR_ID 0
-		#define TEMP_MINUS_ID 1
-		#define TEMP_PLUS_ID 2
-		#define START_STOP_ID 3
-
-		#define CHANGE_START_STOP_TIMER_ID 4
-		#define ADD_HOURS_ID 5
-		#define ADD_MINUTES_ID 6
-		#define ADD_SECONDS_ID 7
-
-		// What is the resistor tolerance?
-		// %, multiplied by 10, 5% should be 50
-		#define SWITCHES_RESISTOR_TOLERANCE 50
-
-		// Value of R2 in the voltage divider, the resistor tied from SWITCHES_INPUT_PIN to ground
-		#define SWITCHES_R2 20000 // 20k
-
-		// R1 values, resistors tied from SWITCHES_INPUT_PIN to every pushbutton
-		// The button input then should be tied to VCC
-		const uint32_t SwitchesR1[SWITCHES_AMOUNT]
-		{
-			100000,	// 100k
-			68000,	// 68k
-			33000,	// 33k
-			20000,	// 20k
-			10000,	// 10k
-			6800,	// 6k8
-			3300,	// 3k3
-			0		// 0 ohms (switch tied to VCC without any resistor)
-		};
-
-	// Interface delays
-
-		// How many time the info screen will be showed at startup
-		#define AFTER_WELCOME_DELAY 500
-
-		// Time to wait after every Temp++/-- button press WITHOUT READING
-		#define TEMP_BUTTON_DELAY 50
-
-		// Time to wait after every Start/Stop button press WITHOUT READING
-		// This will avoid multiple presses with only one click
-		#define START_STOP_BUTTON_DELAY 500
-		
-		// Time to wait after every ChangeDir button press WITHOUT READING
-		// This will avoid multiple presses with only one click
-		#define CHANGE_DIR_BUTTON_DELAY 500
-
-		// How many time should be wait between every blinking char in the screen
-		#define BLINKING_CHARACTER_DELAY 500
-
-	// Button thresholds
-
-		// How many time should the user press Temp++/-- in order to be detected as a press?
-		#define TEMP_BUTTON_THRESHOLD 25
-
-		// The same, with Start/Stop button
-		#define START_STOP_BUTTON_THRESHOLD 25
-
-		// Yeah, the same, you should know what button we're talking about..
-		// OKAY, RIGHT, I'M TIRED OF USING CTRL+C/CTRL+V
-		#define CHANGE_DIR_BUTTON_THRESHOLD 25
-
-	// Temperature limits and default data
-
-		// Temperature limits in the alarm config
-		// Should be between -127 and 127, as AlarmTemperature is an 8-bit integer
-		#define HIGHEST_TEMPERATURE 100
-		#define LOWEST_TEMPERATURE -20
-
-		// Default AlarmTemperature at startup if the value stored in the EEPROM is not valid
-		// Also between -127 and 127
-		#define START_TEMPERATURE 25
-
-	// LCD configuration
-
-		#define LCD_I2C_ADDRESS 0x27 // The address of the I2C Port Expander
+		#define LCD_I2C_ADDRESS 0x27
 
 		#define LCD_COLUMNS 20
 		#define LCD_ROWS 4
@@ -115,29 +35,124 @@
 
 		#define LCD_ALARM_DISABLED '-'
 
-		#define LCD_ALARM_DIRECTION_0 '>' // >
-		#define LCD_ALARM_DIRECTION_1 '<' // <
+		#define LCD_ALARM_DIRECTION_0 '>'
+		#define LCD_ALARM_DIRECTION_1 '<'
 
 		#define LCD_DEGREE_CHAR (char) 223
-
-	// Timer configuration
-
-		// The first 2 LCD rows are used by the temperature alarm
-		#define TIMER_AMOUNT LCD_ROWS - 2
-
-		// Timer limit in seconds
-		#define TIMER_LIMIT 359999 // 99 hours, 59 minutes, 59 seconds
-
-	// Other config
 
 		// Comment it to disable
 		#define LCD_BACKLIGHT_ON
 
-		// Where is stored the last temperature?
-		#define EEPROM_TEMPERATURE_ADDRESS 0
+	/**
+	 * Switches
+	 * 
+	 * ID: The index of the resistor configured in OneWireSwitches, which
+	 * is tied to the switch we want to control. 
+	 * THRESHOLD: The shortest pulse detected as a press by the firmware. 
+	 * DELAY: Time (ms) to wait after the press without reading any button. 
+	 */
 
-		// Where is stored the last alarm direction?
+		#define CHANGE_DIR_ID 0
+		#define CHANGE_DIR_BUTTON_THRESHOLD 25
+		#define CHANGE_DIR_BUTTON_DELAY 500
+		
+		#define TEMP_MINUS_ID 1
+		#define TEMP_PLUS_ID 2
+		#define TEMP_BUTTON_THRESHOLD 25
+		#define TEMP_BUTTON_DELAY 50
+
+		#define START_STOP_ID 3
+		#define START_STOP_BUTTON_DELAY 500
+		#define START_STOP_BUTTON_THRESHOLD 25
+
+	/**
+	 * OneWireSwitches configuration
+	 * This lib allows us to read a lot of switches using only one analog input. 
+	 * It uses a simple voltage divider multiplexed with every switch. 
+	 * 
+	 * Then: 
+	 * 1. Put a resistor (R2) between the analog pin and GND. 
+	 * 2. Put a resistor (R1s) between the output of every
+	 *    switch and the analog pin. 
+	 *    The other side of the switch should be tied to VCC. 
+	 * 3. Configure it all here, including the resistor tolerance, 
+	 *    multiplied by 10 (usually 50 (5%))
+	 * 
+	 * The library will make the calculations and now you can read all those
+	 * switches by using readKey(R1Index). R1 index is the number of resistor
+	 * configured here, starting off by 0. 
+	 * 
+	 * See https://learn.sparkfun.com/tutorials/voltage-dividers if you want
+	 * to understand how this all works!
+	 */
+
+		#define SWITCHES_INPUT_PIN A2
+
+		#define SWITCHES_AMOUNT 8
+
+		#define SWITCHES_RESISTOR_TOLERANCE 50
+
+		#define SWITCHES_R2 20000 // 20k
+
+		const uint32_t SwitchesR1[SWITCHES_AMOUNT]
+		{
+			100000,	// 100k
+			68000,	// 68k
+			33000,	// 33k
+			20000,	// 20k
+			10000,	// 10k
+			6800,	// 6k8
+			3300,	// 3k3
+			0		// 0 ohms (switch output tied directly to the input)
+		};
+
+	/**
+	 * EEPROM
+	 * 
+	 * The EEPROM can store information for a loong time. 
+	 * Here, we define in which direction will we store the 
+	 * last used temperature and alarm direction. 
+	 */
+
+		#define EEPROM_TEMPERATURE_ADDRESS 0
 		#define EEPROM_DIRECTION_ADDRESS 1
+
+	/**
+	 * Alarm
+	 * 
+	 * Here is the configuration of the alarm in itself. 
+	 */
+
+		// How many time the info screen will be showed at startup
+		#define AFTER_WELCOME_DELAY 500
+
+		// To do: PWM output
+		#define BUZZER_PIN 1
+
+	/**
+	 * MODULE: Temperature
+	 * 
+	 * This module is a temperature-triggered alarm, it can alert you
+	 * when something reachs the given temperature. 
+	 * 
+	 * The sensor is a DS18B20, but the DS18S20 and the DS1820 should work too. 
+	 * It is connected through a OneWire bus. 
+	 */
+
+		// It disturbs the SPI signals when programming if tied to some SPI pin
+		#define ONE_WIRE_BUS 3
+
+		// Between -127 and 127, as TemperatureAlarmThreshold is an 8-bit integer
+		#define HIGHEST_TEMPERATURE 100
+		#define LOWEST_TEMPERATURE -20
+
+		// Default at startup if the value stored in the EEPROM is not valid
+		// Also between -127 and 127
+		#define START_TEMPERATURE 25
+
+		// Some characters blink on the screen on certain situations. 
+		// What should be the delay between everyone of them?
+		#define BLINKING_CHARACTER_DELAY 500
 
 	/**
 	 * END CONFIG
