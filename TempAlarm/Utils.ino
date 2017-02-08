a07c515b3ccf75b1b2fd52d4e7d5b4131a32beac
Fermín Olaiz <ferminolaiz@gmail.com>
2017-02-08 00:35:29 -0300

    Forgot to add :P

--- TempAlarm/Utils.ino
+++ TempAlarm/Utils.ino
@@ -1,4 +1,23 @@
+/**
+ * Some functions used by the project
+ */
 
+	// Reads a pulse from a button and returns the time since the press
+	// Returns 0 if the button is not pressed
+	uint16_t ReadPulse(uint8_t KeyIndex, uint16_t Timeout)
+	{
+		uint16_t Time = 0;
+
+		while(Switches.readKey(KeyIndex) == HIGH && Time < Timeout)
+		{
+			Time++;
+			delay(1);
+		}
+
+		return Time;
+	}
+
+	// This function clears only one row of the LCD
 	void ClearLCDRow(int8_t RowIndex)
 	{
 		LCD.setCursor(0, RowIndex);
