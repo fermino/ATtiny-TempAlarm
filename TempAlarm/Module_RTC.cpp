57f144606b42c076da929b8010a5038ea4bb39a1
Fermín Olaiz <ferminolaiz@gmail.com>
2017-02-23 22:40:18 -0300

    Adding environment temperature (DS3231 sensor)

--- TempAlarm/Module_RTC.cpp
+++ TempAlarm/Module_RTC.cpp
@@ -7,6 +7,11 @@
 		LCD->setCursor(17, 0);
 		LCD->print(':');
 
+		LCD->setCursor(18, 2);
+		// Warning, we're using Temperature's defines
+		LCD->print(TEMPERATURE_DEGREE_CHAR);
+		LCD->print('C');
+
 		if(Switches->readKey(RTC_BUTTON_CONFIGURE_ID))
 		{
 			loop();
@@ -73,6 +78,9 @@
 		printZerofill(Time[RTC_MINUTES]);
 		LCD->setCursor(18, 0);
 		printZerofill(Time[RTC_SECONDS]);
+
+		LCD->setCursor(15, 2);
+		LCD->print(getTemperature());
 	}
 
 	void RTCAlarm::getTime()
